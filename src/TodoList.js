f0755ab4a4be5357df4e6c5f0f9e67501e90865b
Sairam <sairamkammili@gmail.com>
2018-11-28 23:46:49 -0800

    Using filter , added remove todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -9,6 +9,7 @@ class TodoList extends React.Component{
             items : []
         }
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
 
     addItem(e){
@@ -28,6 +29,16 @@ class TodoList extends React.Component{
         console.log(this.state.items);
         e.preventDefault();
     }
+    
+    deleteItem(key){
+        var filteredItems = this.state.items.filter(function(item){
+            return (item.key!== key)
+        });
+
+        this.setState({
+            items : filteredItems
+        });
+    }
     render(){
         return(
             <div className="ToDoListMain">
@@ -38,7 +49,8 @@ class TodoList extends React.Component{
                         <button type="submit">Add</button>
                     </form>
                 </div>
-            <TodoItems entries={this.state.items}/>
+            <TodoItems entries={this.state.items}
+            delete={this.deleteItem}/>
             </div>
         )
     }
